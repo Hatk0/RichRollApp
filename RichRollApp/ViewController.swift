a52193d39f5720d6d8028cfbe771d70e4a12a98a
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-01-05 16:23:36 +0300

    Initial Commit

---
+++ RichRollApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichRollApp
+//
+//  Created by Dmitry Yastrebov on 05.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
