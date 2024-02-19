0dbe4a984ce29cb01f30433ba65623670bef8900
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-02-19 14:04:48 +0300

    Create JSON files

--- RichRollApp/Sources/View/More/Model/More.swift
+++ RichRollApp/Sources/View/More/Model/More.swift
@@ -1,59 +1,6 @@
 import Foundation
 
-struct More: Hashable {
+struct More: Codable {
     var image: String
     var title: String
-}
-
-extension More {
-    static let moreArray = [
-        
-        // Адреса и карты
-        [
-            More(
-            image: "point",
-            title: "Адреса"
-        ),
-         More(
-            image: "cards",
-            title: "Карты"
-         )
-        ],
-        
-        // Мои промокоды
-        [
-            More(
-            image: "promocode",
-            title: "Мои промокоды"
-            )
-        ],
-        
-        // Избранное, настройки, условия доставки, рестораны, связаться с нами, о приложении
-        [
-            More(
-                image: "favourites",
-                title: "Избранное"
-            ),
-            More(
-                image: "settings",
-                title: "Настройки"
-            ),
-            More(
-                image: "car",
-                title: "Условия доставки"
-            ),
-            More(
-                image: "restaurants",
-                title: "Рестораны"
-            ),
-            More(
-                image: "message",
-                title: "Связаться с нами"
-            ),
-            More(
-                image: "info",
-                title: "О приложении"
-            )
-        ]
-    ]
 }
--- RichRollApp/Sources/View/Stocks/Model/Stocks.swift
+++ RichRollApp/Sources/View/Stocks/Model/Stocks.swift
@@ -1,62 +1,7 @@
 import Foundation
 
-struct Stocks: Hashable {
+struct Stocks: Codable {
     var image: String
     var title: String
     var description: String
-}
-
-extension Stocks {
-    static let modelsArray = [
-        Stocks(
-            image: "dragon",
-            title: "Сет дракона",
-            description: "ограниченная версия - только в меню доставки!"
-        ),
-        Stocks(
-            image: "discount",
-            title: "-20% только для вас",
-            description: ""
-        ),
-        Stocks(
-            image: "taste",
-            title: "Насладитесь вкусом",
-            description: "с Rich"
-        ),
-        Stocks(
-            image: "forel",
-            title: "Новое специальное меню",
-            description: "«Форель. Погружение»"
-        ),
-        Stocks(
-            image: "onigiri",
-            title: "Онигири",
-            description: "Новинка в «RichRolls»!"
-        ),
-        Stocks(
-            image: "dragondays",
-            title: "Счастливые новогодние дни",
-            description: "Повышенная скидка по карте лояльности, подарки и сюрпризы!"
-        ),
-        Stocks(
-            image: "burger",
-            title: "Вместе выгоднее",
-            description: "При заказе любого бургера из меню - Барэисе Маруяки по цене 103 руб."
-        ),
-        Stocks(
-            image: "loyalty",
-            title: "Получайте и удерживайте -20% в ресторанах!",
-            description: ""
-        ),
-        Stocks(
-            image: "deliverytips",
-            title: "Чаевые курьерам",
-            description: "теперь можно поблагодарить курьера в приложении!"
-        ),
-        Stocks(
-            image: "deliverydiscount",
-            title: "-20% на доставку",
-            description: ""
-        )
-    ]
 }
